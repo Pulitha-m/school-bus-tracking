898b4112c2b93e340c1ace8494362ea4f8cb9e8c
Pulitha Marasinghe <pulithamarasinghe@gmial.com>
2025-03-09 17:27:18 +0530

    Configured tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Heloo</h1>
+      <h1 className="text-red-500">Heloo</h1>
       <h2>Helloooo</h2>
       <h1>Heloo</h1>
       <h2>Helloooo</h2>
